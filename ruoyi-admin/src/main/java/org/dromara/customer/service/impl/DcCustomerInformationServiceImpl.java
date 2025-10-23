b18facb24b5cb80807e03dcf7c1de950723590a1
xhc <2913597577@qq.com>
2025-10-23 20:47:06 +0800

    优化

--- ruoyi-admin/src/main/java/org/dromara/customer/service/impl/DcCustomerInformationServiceImpl.java
+++ ruoyi-admin/src/main/java/org/dromara/customer/service/impl/DcCustomerInformationServiceImpl.java
@@ -93,6 +93,7 @@ public class DcCustomerInformationServiceImpl implements IDcCustomerInformationS
         lqw.eq(bo.getActionType() != null, DcCustomerInformation::getActionType, bo.getActionType());
         lqw.eq(bo.getTransferId() != null, DcCustomerInformation::getTransferId, bo.getTransferId());
         lqw.eq(bo.getIsAssigned() != null, DcCustomerInformation::getIsAssigned, bo.getIsAssigned());
+        lqw.eq(bo.getCustomerType() != null, DcCustomerInformation::getCustomerType, bo.getCustomerType());
         return lqw;
     }
 
